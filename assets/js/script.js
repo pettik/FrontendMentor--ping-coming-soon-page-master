0fd31155e27a14d1faa2bd4dc1b5ac137fe80190
Petr Bednarski <petrbednarski@gmail.com>
2023-08-26 22:01:13 +0200

    new updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    const emailInput = document.getElementById('email');
-    const alertParagraph = document.querySelector('.alert');
-  
-    document.getElementById('btn').addEventListener('click', function () {
-      const emailValue = emailInput.value.trim();
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-      if (emailValue === '' || !emailPattern.test(emailValue)) {
-        emailInput.classList.add('red');
-        alertParagraph.style.visibility = 'visible';
-      } else {
-        emailInput.classList.remove('red');
-        alertParagraph.style.visibility = 'hidden';
-      }
-    });
-  
-    emailInput.addEventListener('input', function () {
-      emailInput.classList.remove('red');
-      alertParagraph.style.visibility = 'hidden';
-    });
+  const emailInput = document.getElementById('email');
+  const alertParagraph = document.querySelector('.alert');
+  const mainContent = document.querySelector('main');
+
+  document.getElementById('btn').addEventListener('click', function () {
+    const emailValue = emailInput.value.trim();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailValue === '' || !emailPattern.test(emailValue)) {
+      mainContent.classList.add('red');
+      alertParagraph.style.display = 'block';
+    } else {
+      mainContent.classList.remove('red');
+      aalertParagraph.style.display = 'none';
+    }
   });
+
+  // emailInput.addEventListener('input', function () {
+  //   mainContent.classList.remove('red');
+  //   alertParagraph.style.display = 'block';
+  // });
+});
